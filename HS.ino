d3b12b72bb7fd924d3cf23a7e59b54320f218d53
Sebastian Godoy <110515128+sgodoyo@users.noreply.github.com>
2023-05-17 10:16:39 -0400

    add code for oled screen c12

--- HS.ino
+++ HS.ino
@@ -1,3 +1,12 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // ancho de la pantalla OLED
+#define SCREEN_HEIGHT 64 // altura de la pantalla OLED
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 const int sensorPin = A0;
 const int humedadAire = 822;
 const int humedadAgua = 420;
@@ -6,6 +15,11 @@ int porcentajeHumedad;
 void setup() 
 {
   Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.clearDisplay();
 }
 
 void loop() 
@@ -21,5 +35,14 @@ void loop()
    Serial.print(porcentajeHumedad);
    Serial.println("%"); 
 
-   delay(3000); // Pausa de 1 segundo
+   display.clearDisplay();
+   display.setTextSize(2);
+   display.setTextColor(WHITE);
+   display.setCursor(0, 0);
+   display.println("Humedad:");
+   display.print(porcentajeHumedad);
+   display.println("%");
+   display.display();
+
+   delay(3000); // Pausa de 3 segundos
 }
