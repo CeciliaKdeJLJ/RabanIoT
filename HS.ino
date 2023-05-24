bdd787adf19a37da30ece683c7812a5d8518362c
Sebastian Godoy <110515128+sgodoyo@users.noreply.github.com>
2023-05-23 23:36:27 -0400

    add light sensor read and echo oled

--- HS.ino
+++ HS.ino
@@ -1,48 +1,73 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <BH1750.h>
 
 #define SCREEN_WIDTH 128 // ancho de la pantalla OLED
 #define SCREEN_HEIGHT 64 // altura de la pantalla OLED
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_ADDR      0x3C
+#define SENSOR_ADDR    0x23
+#define SENSOR_PIN     A0
+#define HUMEDAD_AIRE   822
+#define HUMEDAD_AGUA   420
+#define DELAY_TIME     3000
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
-const int sensorPin = A0;
-const int humedadAire = 822;
-const int humedadAgua = 420;
-int porcentajeHumedad;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+BH1750 lightMeter(SENSOR_ADDR);
 
 void setup() 
 {
   Serial.begin(9600);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) { 
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+    for(;;); // Don't proceed, loop forever
   }
   display.clearDisplay();
 }
 
 void loop() 
 {
-   int humedad = analogRead(sensorPin);
-   Serial.println(humedad);
-   
-   porcentajeHumedad = map(humedad, humedadAire, humedadAgua, 0, 100);
-   if (porcentajeHumedad > 100) {
-     porcentajeHumedad = 100;
-   }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
 
-   Serial.print(porcentajeHumedad);
-   Serial.println("%"); 
+  // Humidity processing
+  int humedad = analogRead(SENSOR_PIN);
+  int porcentajeHumedad = map(humedad, HUMEDAD_AIRE, HUMEDAD_AGUA, 0, 100);
+  if (porcentajeHumedad > 100) {
+    porcentajeHumedad = 100;
+  }
 
-   display.clearDisplay();
-   display.setTextSize(2);
-   display.setTextColor(WHITE);
-   display.setCursor(0, 0);
-   display.println("Humedad:");
-   display.print(porcentajeHumedad);
-   display.println("%");
-   display.display();
+  // Light processing
+  if(!lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE)) {
+    Serial.println(F("Could not begin BH1750"));
+    for(;;); // Don't proceed, loop forever
+  }
+  float lux = lightMeter.readLightLevel();
 
-   delay(3000); // Pausa de 3 segundos
+  // Display humidity and light data
+  Serial.print("Humedad: ");
+  Serial.print(porcentajeHumedad);
+  Serial.println("%"); 
+  Serial.print("Luz: ");
+  Serial.print(lux);
+  Serial.println(" lux");
+
+  // Display Humidity
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println("Humedad:");
+  display.print(porcentajeHumedad);
+  display.println("%");
+
+  // Display Light
+  display.setTextSize(2);
+  display.setCursor(0, 32);
+  display.println("Luz:");
+  display.print(lux);
+  display.println(" lux");
+
+  display.display();
+
+  delay(DELAY_TIME);
 }
